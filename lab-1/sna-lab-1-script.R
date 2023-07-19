2946c460a7c43af5b5b1ba52dfa5c0ae0ba9045a
sbkellogg <sbkellog@ncsu.edu>
2023-07-19 06:58:36 -0400

    scavenger hunt network

---
+++ lab-1/sna-lab-1-script.R
@@ -0,0 +1,11 @@
+# Shaun Kellogg
+# SNA Lab 1 
+# July 18, 2023 
+
+
+# PREPARE #########################
+
+library(readxl)
+
+## Load Packages #### 
+
---
+++ sna-intro/laser-network.R
@@ -0,0 +1,26 @@
+
+
+library(tidyverse)
+library(tidygraph)
+library(ggraph)
+
+scholar_edges <- read_csv("sna-intro/data/scholar-edges.csv")
+scholar_nodes <- read_csv("sna-intro/data/scholar-nodes.csv")
+
+scholar_network <- tbl_graph(edges = scholar_edges, # specifies edges
+                             nodes = scholar_nodes,
+                             directed = TRUE) # specifies directionality
+
+scholar_network
+
+
+ggraph(scholar_network, layout = "stress") + 
+  geom_edge_link(arrow = arrow(length = unit(1, 'mm')), 
+                 end_cap = circle(3, 'mm'),
+                 start_cap = circle(3, 'mm'),
+                 alpha = .1) +
+  geom_node_point(aes(size = local_size(),
+                      color = gender)) +
+  geom_node_text(aes(label = id),
+                 repel=TRUE) +
+  theme_graph()
